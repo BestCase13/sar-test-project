b16efec4c22111ffe2e9e90255e80627e3447a54
BestCase13 <panchyshyn.andrew@gmail.com>
2021-02-18 05:27:43 +0200

    Add contact us form

--- src/app/features/contact-us/contact-us.component.ts
+++ src/app/features/contact-us/contact-us.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrls: ['./contact-us.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./contact-us.component.scss']
 })
 export class ContactUsComponent implements OnInit {
+
+phoneNumberRegularExpression = new RegExp(`\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*`);
+
+  contactUsForm = new FormGroup({
+    fullNameInput: new FormControl('', [Validators.required]),
+    emailInput: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumberInput: new FormControl('', [Validators.required, Validators.pattern(this.phoneNumberRegularExpression)]),
+  });
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  submitForm(): void { }
 }
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 import { ContactUsComponent } from './contact-us/contact-us.component';
@@ -15,6 +15,7 @@ import { PostsComponent } from './posts/posts.component';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule
   ]
 })
